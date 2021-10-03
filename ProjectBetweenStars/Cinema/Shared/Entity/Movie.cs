1398fed9b0e39167f761acd9d472733310b296be
juan s. villegas <juanvjaramillo@gmail.com>
2021-10-03 01:00:50 -0500

    Ciclos For y Foreach Implementados, Sin clase de Sábado

---
+++ ProjectBetweenStars/Cinema/Shared/Entity/Movie.cs
@@ -0,0 +1,18 @@
+using System;
+namespace Cinema.Shared.Entity
+{
+    public class Movie
+    {
+        public string Title {get;set;}
+        public string Synopsis {get;set;}
+        public DateTime ReleaseDate {get;set;}
+        public string UriImage {get;set;}
+        public string UriTriler {get;set;}
+        public string Runtime {get;set;}
+        public string Genre {get;set;}
+        public string Cast {get;set;}
+        public double Rating {get;set;}
+        public bool IsNowPlaying {get;set;}
+    };
+}
+
