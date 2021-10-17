da5f7a6c6bc690215affbbcc4a223a5ad46a73bf
juan s. villegas <juanvjaramillo@gmail.com>
2021-10-16 19:50:26 -0500

    Se agrega selector multiple y se dejan las vistas completas del CRUD, falta parte de la de movies

---
+++ BlazorProjectIII/Client/Model/SelectorMultipleModel.cs
@@ -0,0 +1,14 @@
+namespace BlazorProjectIII.Client.Model
+{
+    public class SelectorMultipleModel
+    {
+        public SelectorMultipleModel(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set;}
+    }
+}
--- BlazorProjectIII/Shared/Entity/Movie.cs
+++ BlazorProjectIII/Shared/Entity/Movie.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System;
+
 namespace BlazorProjectIII.Shared.Entity
 {
     public class Movie
@@ -7,24 +9,30 @@ namespace BlazorProjectIII.Shared.Entity
         public int Id { get; set;}
         [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Name { get; set;}
+        public bool IsNowPlaying { get; set;}
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Synopsis { get; set;}
         public string Poster { get; set;}
         [Required(ErrorMessage = "El campo {0} es requerido")]
         public DateTime Premier { get; set;}
+        [Required(ErrorMessage = "El campo {0} es requerido")]
         public string Triler { get; set;}
         public double Rating { get; set;}
-        public bool IsNowPlaying { get; set;}
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public List <CategoryMovie> CategoriesMovie { get; set;} = new List <CategoryMovie> ();
         public string ShortName {
             get{
                 if (string.IsNullOrWhiteSpace(Name)){
                     return null;
-                } if (Name.Length > 60) {
+                }
+                if (Name.Length > 60) {
                     return Name.Substring(0, 60) + "...";
                 } else {
                     return Name;
                 }
             }
         }
-    };
+    }
 }
 
